e4c25c31a7738581133562cfd49080616d78dca1
Santiago Torres <torres_santiago@javeriana.edu.co>
2019-10-16 16:12:22 -0500

    Update main.c

--- FFTDITEnC/FFTDIT/main.c
+++ FFTDITEnC/FFTDIT/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <inttypes.h>
 #include <math.h>
-#include "Num_Complejos.h"
+#include <complex.h>
 
 #define DATA_TYPE uint16_t
 #define VECT_SIZE 8
@@ -12,69 +12,44 @@
 
 
 int Bit_Reverse(DATA_TYPE *in, DATA_TYPE *out);
-uint16_t fftdit(COMPLEX *);
+uint16_t fftdit(complex *);
 
-DATA_TYPE Signal[NVAL] = {5,5,5,5,5,5,5,5};
+DATA_TYPE Signal[NVAL] = {1,-1,1,-1,1,-1,1,-1};
 
 int main(){
-
     // BIT REVERSE
     DATA_TYPE SReverse[NVAL];
-    COMPLEX SReverseComplex[NVAL], FFTVector[NVAL];
-    (Bit_Reverse(&Signal[0], &SReverse[0]))? printf("EXITOS EN BIT REVERSE!\n") :printf("FATAL ERROR EN BIT REVERSE\n");
-    int p;
-    for(p = 0; p<VECT_SIZE; p++){
-        printf("%d \t", SReverse[p]);
-    }
-    printf ("\n");
-
+    complex SReverseComplex[NVAL], FFTVector[NVAL];
+    (Bit_Reverse(&Signal[0], &SReverse[0]));
     // CONVERTIR DATOS A POLARES
     // COPIAR DATOS EN OTRO VECTOR PARA NO PERDERLO
     uint16_t i = 0;
     for(i = 0; i < NVAL; i++){
-        SReverseComplex[i].x = (double)SReverse[i];
-        SReverseComplex[i].m = (double)SReverse[i];
-        SReverseComplex[i].y = 0;
-        SReverseComplex[i].a = 0;
-
+        SReverseComplex[i] = (double complex)SReverse[i];
         FFTVector[i] =  SReverseComplex[i];
     }
-    for(p = 0; p<VECT_SIZE; p++){
-        printf("%f \t", FFTVector[p].x);
+    int k = 0;
+    for(k = 0; k<VECT_SIZE; k++){
+        printf("SigIn[%d] = %.2f %+.2fi\n",k, creal(SReverseComplex[k]), cimag(SReverseComplex[k]));
     }
-    printf ("\n");
-
-
+    printf("\n\n\n");
     // FFT
-    ((fftdit(FFTVector))? printf("EXITOS EN FFT!\n") :printf("FATAL ERROR EN FFT\n"));
+    fftdit(FFTVector);
 
-
-    printf ("Parte Real \n ");
+    printf("\n\n\n");
+    int p = 0;
     for(p = 0; p<VECT_SIZE; p++){
-        printf("%0.2f \t", FFTVector[p].x);
+        printf("FFT[%d] = %.2f %+.2fi\n",p, creal(FFTVector[p]), cimag(FFTVector[p]));
     }
-    printf ("\n Parte Imaginaria \n ");
-    /**/
-    for(p = 0; p<VECT_SIZE; p++){
-        printf("%0.2f \t", FFTVector[p].y);
-    }
-    printf ("\n");
-
-    //printf("%ff", SReverseComplex[0].x );
-    //printf("%d\n", Signal[0]);
-
     return 0;
 }
 
 int Bit_Reverse(DATA_TYPE *in, DATA_TYPE *out){
     uint16_t vect_pos = 0, bit = 0;
     uint16_t res_index;
-    //printf("EXITOS\n");
     for(vect_pos = 0; vect_pos<VECT_SIZE; vect_pos++){  // RECORRER POSICIONES DEL VECTOR
         res_index = 0;
-        //printf("vect_pos %d \n", vect_pos);
         for(bit = 0; bit<NUM_BITS; bit++){              // RECORRER BITS DEL INDICE DE CADA POSICION DEL VECTOR
-            //printf("bit %d \t \n", bit);
             if(vect_pos & (1<<bit))                     // PREGUNTAR SI EL BIT DEL INDICE ES 1
               res_index |= (1<<(NUM_BITS-bit-1));       // PONER EN 1 LA POSICION DEL BIT INVERTIDO EN EL INDICE
         }
@@ -83,7 +58,7 @@ int Bit_Reverse(DATA_TYPE *in, DATA_TYPE *out){
     return 1;
 }
 
-uint16_t fftdit(COMPLEX x[]){
+uint16_t fftdit(complex x[]){
     uint16_t Half = 1;
     uint16_t Stage = 1;
     uint16_t Butterfly = 0;
@@ -92,20 +67,26 @@ uint16_t fftdit(COMPLEX x[]){
 
     double r;
 
-    COMPLEX Wn, a;
+    complex Wn, temp;
 
     for(Stage = 1; Stage <= NUM_BITS ; Stage++){
         printf("Stage %d Half %d \n", Stage, Half);
         for(Butterfly = 0; Butterfly < NVAL; Butterfly += pow(2,Stage)){
-            printf("\t Butterfly%d \n", Butterfly);
+            printf("\t Butterfly %d \n", Butterfly);
             for(n = 0; n < Half; n++){
+                printf("\t \t n %d \n", n);
                 pos = n + Butterfly;
+                printf("\t \t \t pos %d \n", pos);
                 r = pow(2,(NUM_BITS - Stage)) * n;
-                Wn = WnCalculator(r);
-                a = ComplexMult(Wn, x[pos+Half]);
-                printf("\t \t \t a %0.3f + %0.3fj \n", (float)a.x, (float)a.y);
-                x[pos]          = ComplexSuma   (x[pos], a); // OPERACIONES
-                x[pos+Half]     = ComplexResta  (x[pos], a);
+                printf("\t \t \t r %0.2f \n", r);
+                Wn = cexp(((I)*(2*M_PI)*r/VECT_SIZE));
+                printf("\t \t \t Wn = %f %+fi\n", creal(Wn), cimag(Wn));
+                printf("\t \t \t x[pos+Half] = %f %+fi\n", creal(x[pos+Half]), cimag(x[pos+Half]));
+                printf("\t \t \t x[pos] = %f %+fi\n", creal(x[pos]), cimag(x[pos]));
+                temp = (Wn * x[pos+Half]);
+                printf("\t \t \t temp = %f %+fi\n", creal(temp), cimag(temp));
+                x[pos]          = (x[pos] + temp); // OPERACIONES
+                x[pos+Half]     = (x[pos] - temp);
             }
         }
         Half *= 2;
